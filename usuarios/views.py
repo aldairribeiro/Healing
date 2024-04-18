c812c9e46a847dc67c66b796cfe44e1c6dd7f18b
aldair Ribeiro <aldair.martis0@gmail.com>
2024-04-18 15:21:29 -0300

    feito a parte de cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,33 +1,60 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+from django.contrib import auth
 
 # Create your views here.
 def cadastro(request):
     if request.method == "GET":
         return render(request, 'cadastro.html')
-    
     elif request.method == "POST":
         username = request.POST.get('username')
         email = request.POST.get('email')
         senha = request.POST.get('senha')
         confirma_senha = request.Post.get('confirma_senha')
+        
+    users = User.objects.filter(username=username)
+
+    if users.exists():
+        messages.add_message(request, constants.ERROR, 'Já existe um usuário com esse username')
+        return redirect('/usuarios/cadastro')
     
     if senha != confirma_senha:
-        print('Erro 2')
-        return redirect('/usuarios/cadastro')
+        messages.add_message(request, constants.ERROR, 'A senha e o confirmar senha deve ser iguais')
     
     if len(senha) < 6:
-        print('Erro 3')
+        messages.add_message(request, constants.ERROR, 'A senha deve possuir pelo menos 6 caracteres')
+        return redirect('/usuarios/cadastro')
+
+    try:    
+        user = User.objects.create_user(
+            username=username,
+            email=email,
+            password=senha
+        )
+        return redirect ('/usuarios/login')
+    except:
+        print('Erro 4')
         return redirect('/usuarios/cadastro')
     
-    users = User.objects.filter(username=username)
-    print('Erro 2')
+def login_view(request):
+    if request.method == "GET":
+        return render(request,'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+        
+        user = auth.authenticate(request, username=username, password=senha)
+        
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
+        return redirect('/usuarios/login')
+
+def sair(request):
+    auth.logout(request)
+    return redirect('/usuarios/login')
     
-    user = User.objects.create_user(
-        username=username,
-        email=email,
-        password=senha
-    )
-    
-    return render(request, 'cadastro.html')
